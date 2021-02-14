e00dcb6c2e2d0fbf7deb2994ee2d536f73afb51b
Ali Muhammad <ali.muhammad.ca@gmail.com>
2021-02-14 15:45:20 +0400

    index page working

--- HCTExam/target/generated-sources/annotations/ae/ac/hct/hctexam/Appuser_.java
+++ HCTExam/target/generated-sources/annotations/ae/ac/hct/hctexam/Appuser_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-13T18:07:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-14T15:33:40")
 @StaticMetamodel(Appuser.class)
 public class Appuser_ { 
 
